27953499c117e92ea4a516224ac9c2145d4219e3
Renan Perez <perez.vr@gmail.com>
2020-11-24 00:43:26 -0300

    importando classes

---
+++ Nubank ASP.NET Core/Controllers/FileController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nubank_ASP.NET_Core.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult ImportFile([FromForm] FileUploadViewModel model)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("upload")]
+        public async Task<IActionResult> Upload([FromForm] FileUploadViewModel model, [FromForm] string member_code)
+        {
+            var file = model.File;
+
+            // ...
+        }
+    }
+}
---
+++ Nubank ASP.NET Core/wwwroot/js/ImportarArquivo.js
@@ -0,0 +1,23 @@
+﻿class ImportarArquivo {
+	import(data) {
+        $.ajax({
+            url: '/file/importfile',
+            type: 'GET',
+            contentType: 'multipart/form-data',
+            data: JSON.stringify(data),
+            headers: headers
+        }).done(function (response) {
+            let itemPedido = response.itemPedido;
+            let linhaDoItem = $('[item-id=' + itemPedido.id + ']')
+            linhaDoItem.find('input').val(itemPedido.quantidade);
+            linhaDoItem.find('[subtotal]').html((itemPedido.subtotal).duasCasas());
+            let carrinhoViewModel = response.carrinhoViewModel;
+            $('[numero-itens]').html('Total: ' + carrinhoViewModel.itens.length + ' itens');
+            $('[total]').html((carrinhoViewModel.total).duasCasas());
+
+            if (itemPedido.quantidade == 0) {
+                linhaDoItem.remove();
+            }
+        });
+	}
+}
